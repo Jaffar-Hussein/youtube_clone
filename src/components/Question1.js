447c822540a4efa40ebb4f04d48ca15d4b553f28
Jaffar Hussein <jaffhussein@icloud.com>
2023-03-04 14:08:00 +0100

    Addded the styling for the list elements

--- src/components/Question1.js
+++ src/components/Question1.js
@@ -14,11 +14,14 @@ export default class Question1 extends Component {
 	//   }
 	handleSubmit = (e) => {
 		e.preventDefault();
-		const todoObject = { name: e.target.todo.value, status: false };
-		this.setState({
-			todos: [todoObject, ...this.state.todos],
-			total: (this.state.total += 1),
-		});
+		if (e.target.todo.value) {
+			const todoObject = { name: e.target.todo.value, status: false };
+			this.setState({
+				todos: [todoObject, ...this.state.todos],
+				total: (this.state.total += 1),
+			});
+		}
+
 		console.log(this.state.todos);
 	};
 	render() {
@@ -39,29 +42,31 @@ export default class Question1 extends Component {
 								name='todo'
 								id='voice-search'
 								// onChange={this.addTodo}
-								className='bg-gray-800 border focus:border-emerald-500  border-gray-300 text-gray-200 text-lg rounded-lg focus:ring-gray-500 focus:border-grey-500 block w-full  p-2.0  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500'
+								className='placeholder:italic placeholder:text-emerald-600 bg-gray-800 border focus:border-emerald-500  border-gray-300 text-gray-200 text-lg rounded-lg focus:ring-gray-500 focus:border-grey-500 block w-full  p-2.0  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-grey-500 dark:focus:border-grey-500'
 								placeholder='Add to do'
 							/>
 						</div>
 						<button
 							type='submit'
-							className='inline-flex items-center py-1.8 px-3 ml-2 text-sm font-medium text-white bg-grey-700 rounded-lg border border-grey-700 hover:bg-grey-800 focus:ring-4 focus:outline-none focus:ring-grey-300 dark:bg-grey-600 dark:hover:bg-grey-700 dark:focus:ring-grey-800 focus:shadow-none'
+							className='inline-flex items-center py-1.8 px-3 ml-2 text-sm font-medium text-white bg-grey-700 rounded-lg border focus:border-emerald-500 border-grey-700 hover:bg-grey-800 focus:ring-4 focus:outline-none focus:ring-grey-300 dark:bg-grey-600 dark:hover:bg-grey-700 dark:focus:ring-grey-800 focus:shadow-none'
 						>
 							Add
 						</button>
 					</form>
 
-					<div className='text-xl text-center pt-5  text-emerald-600  '>
+					<div className='text-2xl text-center pt-5  text-emerald-600  '>
 						{" "}
 						<span className='text-sky-400'>{this.state.pending}</span> remaining
 						out of <span className='text-sky-400'>{this.state.total}</span>
 					</div>
 					{/* List to be iterated over */}
-					<ul class='list-disc text-base text-emerald-600 flex flex-col items-center p-5'>
-						{this.state.todos.map((data) => (
-							<li key={data.name}>{data.name}</li>
-						))}
-					</ul>
+					<div className='flex flex-col items-center '>
+						<ul class='list-disc text-xl list-inside  odd:text-emerald-600 even:text-sky-400 p-5'>
+							{this.state.todos.map((data) => (
+								<li key={data.name}>{data.name}</li>
+							))}
+						</ul>
+					</div>
 					<style>
 						{`
                         .is-done {
